b97f6c3ff8bab8b4c1f5a895979185c01b063e5d
Gustavo Caetano Santos <73549261+Gustavo-caetano@users.noreply.github.com>
2024-10-20 18:24:27 -0300

    fix:corrige timer resetado

--- Assets/BOTAOCOD.cs
+++ Assets/BOTAOCOD.cs
@@ -1,26 +1,23 @@
+using System.Diagnostics;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ToggleVisibility : MonoBehaviour
 {
     public GameObject warningImage; // Arraste a imagem de aviso aqui
+    public GameObject cronometro;
     public Button startButton; // Arraste o botão de start aqui
     public Button menuButton; // Arraste o botão de menu aqui
-
     public bool isHidden = false;
+
+    public Stopwatch stopwatch;
+    public long durationMs = 10000; // 1 minuto em milissegundos
+    public long remainingMs;
 
     void Start()
     {
-        // Adiciona um listener ao botão para chamar o método HideWarning quando for clicado
-        startButton.onClick.AddListener(HideWarning);
-    }
-
-    void HideWarning()
-    {
-        // Esconde a imagem de aviso, o botão de start e o botão de menu
-        warningImage.SetActive(false);
-        startButton.gameObject.SetActive(false);
-        menuButton.gameObject.SetActive(false);
-        isHidden = true;
+        stopwatch = new();
+        remainingMs = durationMs;
     }
 }
--- Assets/power barra.cs
+++ Assets/power barra.cs
@@ -8,7 +8,8 @@ public class ProgressBarLooper : MonoBehaviour
     public GameObject maxPowerObj;
     public GameObject timeObj;
     public GameObject powerObj;
-    public GameObject StartObj;
+    public GameObject cronometroObj;
+    public GameObject botaoStartObj;
     public Image progressBarImage;
 
     private NumberCounter time;
@@ -19,6 +20,8 @@ public class ProgressBarLooper : MonoBehaviour
 
     private WeightHandler _weightHandler;
     private Cronometro _cronometro;
+    private ToggleVisibility _botaoStart;
+    private CronometroScript _contagemRegressiva;
 
     void Awake()
     {
@@ -26,10 +29,15 @@ public class ProgressBarLooper : MonoBehaviour
         time = timeObj.GetComponent<NumberCounter>();
         power = powerObj.GetComponent<IntegerCounterDisplay>();
 
-        if (maxPower == null || time == null || power == null)
+        _contagemRegressiva = cronometroObj.GetComponent<CronometroScript>();
+        _botaoStart = botaoStartObj.GetComponent<ToggleVisibility>();
+        cronometroObj.SetActive(false);
+        if (maxPower == null || time == null || power == null || _contagemRegressiva == null || _botaoStart == null)
         {
             Debug.LogError("Um ou mais componentes estão faltando nos objetos!");
         }
+
+        _botaoStart.startButton.onClick.AddListener(HideWarning);
 
         _weightHandler = new WeightHandler(6000); // Peso máximo definido aqui
         _cronometro = new Cronometro();
@@ -38,7 +46,10 @@ public class ProgressBarLooper : MonoBehaviour
         _webSocketHandler.OnMessageReceived += HandleWebSocketMessage;
         _webSocketHandler.Start();
     }
-
+    void Start()
+    {
+        cronometroObj.SetActive(false);
+    }
     void Update()
     {
         if (!Input.anyKeyDown) return;
@@ -56,10 +67,9 @@ public class ProgressBarLooper : MonoBehaviour
         }
 
         string json = JsonUtility.ToJson(comando);
-        Debug.Log(json);      
+        Debug.Log(json);
         _webSocketHandler.SendMsg(json);
     }
-
 
     private void HandleWebSocketMessage(WebSocketHandler.DataPeso data)
     {
@@ -68,7 +78,6 @@ public class ProgressBarLooper : MonoBehaviour
             StartCoroutine(AnimateProgressBar(data));
         });
     }
-
     private IEnumerator AnimateProgressBar(WebSocketHandler.DataPeso data)
     {
         if (data == null) yield return null;
@@ -86,6 +95,52 @@ public class ProgressBarLooper : MonoBehaviour
 
         yield return new WaitForSeconds(0.01f);
     }
+
+    void HideWarning()
+    {
+        _botaoStart.warningImage.SetActive(false);
+        _botaoStart.startButton.gameObject.SetActive(false);
+        _botaoStart.menuButton.gameObject.SetActive(false);
+
+        cronometroObj.SetActive(true);
+        _botaoStart.isHidden = true;
+
+        _botaoStart.stopwatch.Start();
+        _botaoStart.remainingMs = 60000;
+        _botaoStart.durationMs = 60000;
+        StartCoroutine(UpdateTimer());
+    }
+    private IEnumerator UpdateTimer()
+    {
+        while (_botaoStart.remainingMs > 0)
+        {
+            _botaoStart.remainingMs = _botaoStart.durationMs - _botaoStart.stopwatch.ElapsedMilliseconds;
+
+            if (_botaoStart.remainingMs > 0)
+            {
+                int seconds = Mathf.FloorToInt(_botaoStart.remainingMs % 60000 / 1000);
+                int centiseconds = Mathf.FloorToInt(_botaoStart.remainingMs % 1000 / 10);
+                string textoTempo = $"{seconds:D2}:{centiseconds:D2}";
+                _contagemRegressiva.cronometroTexto.text = textoTempo;
+            }
+            else
+            {
+                _contagemRegressiva.cronometroTexto.text = "00:00";
+                cronometroObj.SetActive(false);
+                _botaoStart.stopwatch.Reset();
+
+                var comando = new ComandoEsp
+                {
+                    Opcao = 3
+                };
+                string json = JsonUtility.ToJson(comando);
+                _webSocketHandler.SendMsg(json);
+            }
+
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
 }
 [Serializable]
 public class ComandoEsp
