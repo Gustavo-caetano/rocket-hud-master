9c1182319e1b279b0873bdc0e55269923202e1a9
Gustavo Caetano Santos <73549261+Gustavo-caetano@users.noreply.github.com>
2024-11-01 16:07:51 -0300

    feat: adiciona toast na adição de notificação ao dar error na inserção no banco

--- Assets/Scripts/Banco.cs
+++ Assets/Scripts/Banco.cs
@@ -31,6 +31,7 @@ public class DataBase
         catch (Exception ex)
         {
             Debug.LogError($"Erro ao inserir documento: {ex.Message}");
+            ToastUtil.ShowToastError("Erro ao inserir documento no banco");
         }
     }
     public void SalvarDadosEmJson(DataTeste dados)
--- Assets/Scripts/ToastUtils.cs
+++ Assets/Scripts/ToastUtils.cs
@@ -1,8 +1,14 @@
-using EasyUI.Toast ;
+using EasyUI.Toast;
 
-class ToastUtil {
+class ToastUtil
+{
     public static void ShowToastError(string msg)
     {
         Toast.Show(msg, 2f, ToastColor.Red, ToastPosition.TopCenter);
+    }
+
+    public static void ShowToastSucesss(string msg)
+    {
+        Toast.Show(msg, 2f, ToastColor.Green, ToastPosition.TopCenter);
     }
 }
